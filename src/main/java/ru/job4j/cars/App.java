f1e42362009b8b5c841355b5b01dd3ec68d4e0b3
serj <sergeykalenikov@gmail.com>
2021-09-08 19:52:29 +0300

    hibernate.cfg.xml

---
+++ src/main/java/ru/job4j/cars/App.java
@@ -0,0 +1,38 @@
+package ru.job4j.cars;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.cars.model.Post;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Post post = new Post();
+            session.save(post);
+
+            List<Post> posts = session.createQuery("from Post ", Post.class).getResultList();
+            for (var el : posts) {
+                System.out.println(el);
+            }
+
+            session.getTransaction().commit();
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
